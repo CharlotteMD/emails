7cabc3d8123bd7cee603839de304994d081ea60b
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-06-21 14:01:26 +0100

    styling to signature preview

--- emails/src/components/BasicForm.js
+++ emails/src/components/BasicForm.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-
-import BasicFormBody from './BasicFormBody';
-
-class BasicForm extends Component {
-  render() {
-    return (
-      <div>
-        <BasicFormBody
-          handleBasicChange={this.handleBasicChange}
-          handleBasicSubmit={this.handleBasicSubmit}
-        />
-      </div>
-    );
-  }
-}
-
-export default BasicForm;
+// import React, { Component } from 'react';
+//
+// import BasicFormBody from './BasicFormBody';
+//
+// class BasicForm extends Component {
+//
+//   state = {
+//     form: {
+//       name: '',
+//       role: '',
+//       email: '',
+//       phone: '',
+//       mobile: ''
+//     }
+//   };
+//
+//   handleBasicChange = ({ target: { name, value }}) => {
+//     const form = Object.assign({}, this.state.form, { [name]: value });
+//     this.setState({ form });
+//     console.log('form info: ', form);
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <BasicFormBody
+//           handleBasicChange={this.handleBasicChange}
+//         />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default BasicForm;
--- emails/src/components/BasicFormBody.js
+++ emails/src/components/BasicFormBody.js
@@ -1,79 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BasicFormBody = ({ handleBasicChange }) => {
+class BasicFormBody extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      role: '',
+      email: '',
+      phone: '',
+      mobile: ''
+    };
+  }
 
-  return (
-    <div>
-      <form className="mainform">
-        <h3>Who, what, and where</h3>
+  handleBasicChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
-        <label className="mainlabel">
-          Full Name:
-          <input type="text" onChange={handleBasicChange}></input>
-        </label>
+  render() {
+    return (
+      <div>
+        <form className="mainform">
+          <h3>Who, what, and where</h3>
 
-        <label className="mainlabel">
-          Role:
-          <input type="text" onChange={handleBasicChange}></input>
-        </label>
-
-        <div className="radio">
           <label className="mainlabel">
-            <h4>Office:</h4>
-            <input type="radio" value="lon" name="office" onChange={handleBasicChange}/>
-            <label>London, United Kingdom</label>
-
-            <input type="radio" value="ny" name="office" onChange={handleBasicChange}/>
-            <label>New York, NY</label>
-
-            <input type="radio" value="ber" name="office" onChange={handleBasicChange}/>
-            <label>Berlin, Germany</label>
-
-            <input type="radio" value="tok" name="office" onChange={handleBasicChange}/>
-            <label>Tokyo, Japan</label>
+            Full Name:
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleBasicChange}></input>
           </label>
-        </div>
 
 
-
-        <h3>How to contact you</h3>
-
-        <label className="mainlabel">
-          Email Address:
-          <input type="text" onChange={handleBasicChange}></input>
-        </label>
-
-        <label className="mainlabel">
-          Telephone number (include dial code):
-          <input type="text" onChange={handleBasicChange}></input>
-        </label>
-
-        <label className="mainlabel">
-          Mobile number (include dial code):
-          <input type="text" onChange={handleBasicChange}></input>
-        </label>
-
-        <h3>Your support/assistant details</h3>
-
-        <div className="radio">
           <label className="mainlabel">
-            <h4>Please select one:</h4>
-            <input type="radio" value="na" name="support" onChange={handleBasicChange}/>
-            <label>Not applicable</label>
-
-            <input type="radio" value="ea" name="support" onChange={handleBasicChange}/>
-            <label>I support another team member(s)</label>
-
-            <input type="radio" value="snr" name="support" onChange={handleBasicChange}/>
-            <label>I have an assistant</label>
+            Role:
+            <input
+              type="text"
+              name="role"
+              value={this.state.role}
+              onChange={this.handleBasicChange}>
+            </input>
           </label>
-        </div>
 
-        <button>Next</button>
+          {/* <div className="radio">
+            <label className="mainlabel">
+              <h4>Office:</h4>
+              <input type="radio" value="lon" name="office" onChange={this.handleBasicChange}/>
+              <label>London, United Kingdom</label>
 
-      </form>
-    </div>
-  );
-};
+              <input type="radio" value="ny" name="office" onChange={this.handleBasicChange}/>
+              <label>New York, NY</label>
+
+              <input type="radio" value="ber" name="office" onChange={this.handleBasicChange}/>
+              <label>Berlin, Germany</label>
+
+              <input type="radio" value="tok" name="office" onChange={this.handleBasicChange}/>
+              <label>Tokyo, Japan</label>
+            </label>
+          </div> */}
+
+
+
+          <h3>How to contact you</h3>
+
+          <label className="mainlabel">
+            Email Address:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleBasicChange}>
+            </input>
+          </label>
+
+          <label className="mainlabel">
+            Telephone number (include dial code):
+            <input
+              type="text"
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleBasicChange}>
+            </input>
+          </label>
+
+          <label className="mainlabel">
+            Mobile number (include dial code):
+            <input
+              type="text"
+              name="mobile"
+              value={this.state.mobile}
+              onChange={this.handleBasicChange}>
+            </input>
+          </label>
+
+          {/* <h3>Your support/assistant details</h3>
+
+          <div className="radio">
+            <label className="mainlabel">
+              <h4>Please select one:</h4>
+              <input type="radio" value="na" name="support" onChange={this.handleBasicChange}/>
+              <label>Not applicable</label>
+
+              <input type="radio" value="ea" name="support" onChange={this.handleBasicChange}/>
+              <label>I support another team member(s)</label>
+
+              <input type="radio" value="snr" name="support" onChange={this.handleBasicChange}/>
+              <label>I have an assistant</label>
+            </label>
+          </div> */}
+
+          <button>Next</button>
+
+        </form>
+      </div>
+    );
+  }
+}
 
 export default BasicFormBody;
